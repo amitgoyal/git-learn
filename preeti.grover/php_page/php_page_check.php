31beab9fe8579eb25c2b11743b3a5f93d3e18862
Preeti Grover <preeti.grover@tekritisoftware.com>
2012-09-18 15:01:21 +0530

    deleted

--- preeti.grover/php_page/php_page_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if ($_SESSION['logged_in']) {
-  echo "<p>". "Welcome ".$_SESSION['session_name']."</p>";
-   } else {
-          header("Location:php_page_fail.php");
-  }?>
-  
--- preeti.grover/php_page/php_page_delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'php_page_check.php';
-include 'php_page_config.php';
-delete($_GET['uid']);
-header('location:php_page_display.php?msg=record deleted.');
-?>
