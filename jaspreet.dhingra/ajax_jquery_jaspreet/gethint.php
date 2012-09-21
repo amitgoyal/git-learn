7332650e16ed85e9fe7390cec7ba350939c33352
tekcpu41 <user@tekcpu41-desktop.(none)>
2012-09-21 17:50:36 +0530

    ajax implemented using jquery

---
+++ jaspreet.dhingra/ajax_jquery_jaspreet/gethint.php
@@ -0,0 +1,22 @@
+<?php
+$host ="localhost";
+$user ="root";
+$pass="redhat";
+$db_name="jaspreet";
+$con=mysql_connect($host,$user,$pass);
+mysql_select_db($db_name);
+if(!$con) {
+   echo "cannot connect to database";
+   return false;
+}
+$u_id=$_GET["txt"];
+$res=mysql_query("SELECT * FROM table1 WHERE uname = '".$u_id."'"); 
+if(mysql_num_rows($res) == 0) {
+   echo "You can take this username and it will be saved for future use!";
+   mysql_query("insert into table1(uname) values('$u_id')");
+}
+else { 
+      echo "Sorry this username is already taken";
+}
+?>
+
